f9feda3ef55a2824d3b3e79ccbadb54ca98143a6
marioschi <mariohieblrauschhr66@gmail.com>
2019-02-27 17:43:13 +0100

    push the last assets

---
+++ HelloWeb-Mario/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+	public void LoadSampleScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+}
+
